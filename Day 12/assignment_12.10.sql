ec2e1c5892a118dfddfdf5f67190f331307f8d94
KANISHK GARG <kanishkgargggc@gmail.com>
2025-07-02 19:29:00 +0530

    Create assignment_12.10.sql

---
+++ Day 12/assignment_12.10.sql
@@ -0,0 +1,1 @@
+#In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi — how many points do customer A and B have at the end of January?
